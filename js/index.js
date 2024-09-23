58beefb9f67d113cfa7dd094a8976c664b79f5af
Santanu Banik <Santanubanik2k@gmail.com>
2024-09-23 11:28:28 +0600

    resuable function added and fresh code and utilites js added sticky navbar header added

---
+++ js/index.js
@@ -0,0 +1,86 @@
+
+
+//history tab functionality
+const historyTab = document.getElementById('history-tab');
+const donateTab = document.getElementById('donate-tab');
+// donation buttons
+const nkBtn = document.getElementById('nk-btn')
+const feniBtn = document.getElementById('feni-btn')
+const quotaBtn = document.getElementById('quota-btn')
+
+// donation card handle qith event listener all
+
+// noakhali
+nkBtn.addEventListener('click',function(){
+    
+  const balance = getBalance('balance')
+  const inputDonate = getInputValue('nk-donate');
+  const nkBalance = getBalance('nk-value')
+  const nkTitle = document.getElementById('nk-title').innerText;
+
+  calculate(inputDonate,balance,nkBalance,'nk-value',nkTitle);
+
+})
+
+
+// feni
+feniBtn.addEventListener('click',function(){
+  
+  const balance = getBalance('balance')
+  const inputDonate = getInputValue('feni-donate');
+  const feniBalance = getBalance('feni-value')
+  const feniTitle = document.getElementById('feni-title').innerText;
+
+  calculate(inputDonate,balance,feniBalance,'feni-value',feniTitle);
+
+})
+
+
+// quota
+quotaBtn.addEventListener('click',function(){
+  
+  const balance = getBalance('balance')
+  const inputDonate = getInputValue('quota-donate');
+  const quotaBalance = getBalance('quota-value')
+  const quotaTitle = document.getElementById('quota-title').innerText;
+
+  calculate(inputDonate,balance,quotaBalance,'quota-value',quotaTitle);
+
+})
+
+// historyTab btn click handles
+  historyTab.addEventListener('click', function () {
+     
+    historyTab.classList.add('rounded-l-md',  'text-secondary', 'bg-buttonColor','btn');
+    historyTab.classList.remove('text-gray-600', 'border');
+    donateTab.classList.remove('btn',  'text-secondary', 'bg-buttonColor');
+    donateTab.classList.add('text-gray-600','border','rounded-md');
+     
+    document.getElementById('donation-section').classList.add('hidden');
+
+     document.getElementById('history-section').classList.remove('hidden');
+
+
+
+  });
+
+  // donateTab btn click handles
+  donateTab.addEventListener('click', function () {
+    donateTab.classList.add('btn',  'text-secondary', 'bg-buttonColor');
+    donateTab.classList.remove('text-gray-600');
+    historyTab.classList.remove('rounded-l-md',  'text-secondary', 'bg-buttonColor','btn');
+    historyTab.classList.add('text-gray-600','rounded-md','border');
+    document.getElementById('history-section').classList.add('hidden');
+    document.getElementById('donation-section').classList.remove('hidden');
+  })
+
+
+
+  //blog html enter and back to home  
+  document.getElementById('blog').addEventListener('click',function () {
+    window.location.href = "blog.html";
+  })
+  
+
+
+
---
+++ js/utilities.js
@@ -0,0 +1,40 @@
+function getInputValue(id){
+    return parseFloat(document.getElementById(id).value);
+}
+
+function getBalance(id){
+    return parseFloat(document.getElementById(id).innerText);
+}
+
+
+function calculate(inputDonate,balance,locationBalance,id,areaTitle){
+
+    //validation
+    if(inputDonate < 1 || isNaN(inputDonate)){
+        alert('Invalid Donation Amount!')
+    }else if(balance < inputDonate){
+        
+        alert("You Don't Have Enough Balance!")
+    }else{
+        my_modal_1.showModal();
+        document.getElementById(id).innerText = (locationBalance + inputDonate).toFixed(2);
+        document.getElementById('balance').innerText = (balance - inputDonate).toFixed(2);
+        history(inputDonate,areaTitle);
+    }
+}
+function history(inputDonate,areaTitle){
+
+     //dynamic html created for history purposes
+     const historyItem = document.createElement("div");
+     historyItem.className =
+              "bg-white p-5 rounded-md border space-y-2";
+
+              historyItem.innerHTML = `<p class = 'text-xl font-bold'>${inputDonate}  TAKA  is <span class = 'text-xl font-bold'>${areaTitle} </span>
+              </p>
+               <p class="text-gray-500">Date: ${new Date().toString().replace(/GMT.*$/, "GMT+0600 (Bangladesh Standard Time)")}</p>`;
+              const historyContainer = document.getElementById("history-list");
+
+              historyContainer.insertBefore(historyItem, historyContainer.firstChild);
+
+}
+
